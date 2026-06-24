f490e8f96a478f50845f0ab9c310b575f1b469b4
Matias <lopezmaty@gmail.com>
2026-06-24 14:20:40 -0300

    se crearon los models

---
+++ recetas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Recetas(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500)
+    imagen = models.ImageField()
+    categorias = models.ManyToManyField(Categorias)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField(default=timezone.now)
