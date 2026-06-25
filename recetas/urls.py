f798cd42e0bc0150ac4408afa77a705079699a15
Matias <lopezmaty@gmail.com>
2026-06-25 15:18:57 -0300

    se creo urls de recetas

---
+++ recetas/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include
+from . import views
+
+urlpatterns = [
+    path('lista/', views.RecetasListView.as_view(), name='lista'),
+    path('detalle/<int:pk>/', views.RecetasDetailView.as_view(), name='detalle'),
+    path('crear/', views.RecetasCreateView.as_view(), name='crear'),
+    path('editar/<int:pk>/', views.RecetasUpdateView.as_view(), name='editar'),
+    path('eliminar/<int:pk>/', views.RecetasDeleteView.as_view(), name='eliminar'),
+
+]
