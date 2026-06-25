42580dc54e6b89b10a2299ac12fb8db7f6c71de8
Matias <lopezmaty@gmail.com>
2026-06-25 12:10:10 -0300

    se configuro admin

--- recetas/admin.py
+++ recetas/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+
+@admin.register(models.Categorias)
+class CategoriasAdmin(admin.ModelAdmin):
+    fields = ('nombre',)
+
+@admin.register(models.Recetas)
+class RecetasAdmin(admin.ModelAdmin):
+    fields = ('titulo', 'descripcion', 'imagen', 'categorias', 'autor', 'fecha_hora')
+    list_filter = ('categorias', 'autor',)
+    search_fields = ('titulo',)
+    
+
